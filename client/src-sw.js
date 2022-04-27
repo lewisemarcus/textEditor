1a191b053ce8eadb102045f6a4a89cb693989329
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-04-27 12:51:05 -0700

    succesful page load

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,45 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes")
+const { CacheFirst } = require("workbox-strategies")
+const { registerRoute } = require("workbox-routing")
+const { CacheableResponsePlugin } = require("workbox-cacheable-response")
+const { ExpirationPlugin } = require("workbox-expiration")
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute")
+const { request } = require("express")
 
-precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(self.__WB_MANIFEST)
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
+    cacheName: "page-cache",
+    plugins: [
+        new CacheableResponsePlugin({
+            statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+        }),
+    ],
+})
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
+    urls: ["/index.html", "/"],
+    strategy: pageCache,
+})
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache)
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+    ({ request }) => request.destination === "image",
+    new CacheFirst({
+        cacheName: "my-image-cache",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            }),
+        ],
+    }),
+)
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path")
 const { InjectManifest } = require("workbox-webpack-plugin")
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// DONE: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
     return {
@@ -13,14 +13,81 @@ module.exports = () => {
             main: "./src/js/index.js",
             install: "./src/js/install.js",
         },
+        devServer: {
+            hot: true,
+            static: "./dist",
+            open: false,
+        },
+
         output: {
             filename: "[name].bundle.js",
             path: path.resolve(__dirname, "dist"),
+            clean: true,
         },
-        plugins: [],
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Client Server",
+                template: "./index.html",
+            }),
+            new InjectManifest({
+                swSrc: "./src-sw.js",
+                swDest: "src-sw.js",
+            }),
+            new WebpackPwaManifest({
+                fingerprints: false,
+                inject: true,
+                name: "Client",
+                short_name: "null",
+                description: "empty",
+                background_color: "#225ca3",
+                theme_color: "#225ca3",
+                start_url: "/",
+                publicPath: "/",
+                icons: [
+                    {
+                        src: path.resolve("src/images/logo.png"),
+                        sizes: [96, 128, 192, 256, 384, 512],
+                        destination: path.join("assets", "icons"),
+                    },
+                ],
+            }),
+        ],
 
         module: {
-            rules: [{ test: /\.css$/, use: "css-loader" }],
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"],
+                            plugins: [
+                                "@babel/plugin-proposal-object-rest-spread",
+                                "@babel/transform-runtime",
+                            ],
+                        },
+                    },
+                },
+            ],
+        },
+        resolve: {
+            fallback: {
+                fs: false,
+                net: false,
+                zlib: false,
+                querystring: false,
+                path: false,
+                crypto: false,
+                stream: false,
+                http: false,
+                url: false,
+                util: false,
+            },
         },
     }
 }
